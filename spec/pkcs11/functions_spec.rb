1f85ce1a167fa6fcb9b1feeed97f0f3cac305976
Vincent Touchard <touchardv@gmail.com>
2017-04-12 21:43:26 +0200

    Implement tests using a software HSM.

--- spec/pkcs11/functions_spec.rb
+++ spec/pkcs11/functions_spec.rb
@@ -1,6 +1,15 @@
 require 'spec_helper'
 
 describe Pkcs11 do
+  before(:all) do
+    # brew install softhsm
+    `rm -rf /usr/local/var/lib/softhsm/tokens/*`
+    `softhsm2-util --init-token --slot 0 --id 0x00  --label 'zero' --pin 1234 --so-pin 1234`
+  end
+
+  let(:session_handle_pointer) { FFI::MemoryPointer.new(:ulong) }
+  let(:session_handle) { session_handle_pointer.read_ulong }
+  
   after { Pkcs11.C_Finalize(nil) }
 
   describe '.C_Initialize' do
@@ -47,30 +56,58 @@ describe Pkcs11 do
     describe '.C_GetSlotList' do
       it 'returns CKR_OK' do
         count_pointer = FFI::MemoryPointer.new(:ulong)
-        result = Pkcs11.C_GetSlotList(true, nil, count_pointer)
+        result = Pkcs11.C_GetSlotList(false, nil, count_pointer)
         expect(result).to eq Pkcs11::CKR_OK
       end
 
       it 'returns the slot list and count' do
         count_pointer = FFI::MemoryPointer.new(:ulong)
-        result = Pkcs11.C_GetSlotList(true, nil, count_pointer)
+        result = Pkcs11.C_GetSlotList(false, nil, count_pointer)
         expect(result).to eq Pkcs11::CKR_OK
-        expect(count_pointer.read_ulong).to eq 1
+        expect(count_pointer.read_ulong).to eq 2
 
         slot_ids_pointer = FFI::MemoryPointer.new(:ulong, count_pointer.read_ulong)
-        result = Pkcs11.C_GetSlotList(true, slot_ids_pointer, count_pointer)
+        result = Pkcs11.C_GetSlotList(false, slot_ids_pointer, count_pointer)
         expect(result).to eq Pkcs11::CKR_OK
-        expect(count_pointer.read_ulong).to eq 1
-        expect(slot_ids_pointer[0].read_ulong).to eq 0
+        expect(count_pointer.read_ulong).to eq 2
       end
+    end
+
+    let(:valid_slot) do
+      count_pointer = FFI::MemoryPointer.new(:ulong)
+      result = Pkcs11.C_GetSlotList(false, nil, count_pointer)
+      expect(result).to eq Pkcs11::CKR_OK
+      slot_ids_pointer = FFI::MemoryPointer.new(:ulong, count_pointer.read_ulong)
+      result = Pkcs11.C_GetSlotList(false, slot_ids_pointer, count_pointer)
+      expect(result).to eq Pkcs11::CKR_OK
+      slot_ids_pointer[0].read_ulong
     end
 
     describe '.C_OpenSession' do
       it 'returns CKR_OK' do
-        session_handle = FFI::MemoryPointer.new(:ulong)
-        result = Pkcs11.C_OpenSession(0, Pkcs11::CKF_SERIAL_SESSION, nil, nil, session_handle)
+        result = Pkcs11.C_OpenSession(valid_slot,
+                                      Pkcs11::CKF_SERIAL_SESSION | Pkcs11::CKF_SERIAL_SESSION,
+                                      nil, nil, session_handle_pointer)
         expect(result).to eq Pkcs11::CKR_OK
+
+        result = Pkcs11.C_CloseSession(session_handle)
+        expect(result).to eq Pkcs11::CKR_OK
+      end
+
+      it 'returns CKR_SLOT_ID_INVALID' do
+        result = Pkcs11.C_OpenSession(6666,
+                                      Pkcs11::CKF_SERIAL_SESSION | Pkcs11::CKF_SERIAL_SESSION,
+                                      nil, nil, session_handle_pointer)
+        expect(result).to eq Pkcs11::CKR_SLOT_ID_INVALID
+      end
+    end
+
+    describe '.C_CloseSession' do
+      it 'returns CKR_SESSION_HANDLE_pointer_INVALID' do
+        result = Pkcs11.C_CloseSession(0)
+        expect(result).to eq Pkcs11::CKR_SESSION_HANDLE_pointer_INVALID
       end
     end
   end
 end
+
